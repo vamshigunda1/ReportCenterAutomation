4eac54c4add009e6785de7be2f032c03ed13e5c0
Vamshi Krishna Gunda <vamshi.krishna.gunda@ibm.com>
2019-08-06 15:43:21 +0530

    sanity1

---
+++ reportceneter_pom/src/test/java/com/truven/advantagesuite/sanitytestcases/TC1.java
@@ -0,0 +1,19 @@
+package com.truven.advantagesuite.sanitytestcases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class TC1 {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
